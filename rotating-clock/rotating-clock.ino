ee21f18698e9b2b5d76ccb9986591b63aab417a0
Joeri Gantois <info@JO3RI.be>
2016-05-14 14:14:10 +0200

    Auto Format

--- rotating-clock/rotating-clock.ino
+++ rotating-clock/rotating-clock.ino
@@ -1,13 +1,13 @@
 /*
- DEMO: rotating clock
+  DEMO: rotating clock
 
- TEAM a.r.g.: http://www.team-arg.com/demos.html
+  TEAM a.r.g.: http://www.team-arg.com/demos.html
 
- 2015 - JO3RI
+  2015 - JO3RI
 
- Demo License: MIT : https://opensource.org/licenses/MIT
+  Demo License: MIT : https://opensource.org/licenses/MIT
 
- */
+*/
 
 #include "Arglib.h"
 #include "bitmaps.h"
@@ -25,36 +25,36 @@ unsigned long lastHour = 3600000;
 unsigned long hourInterval = 3600000;
 
 void setup() {
-	arduboy.start();
-	arduboy.clearDisplay();
-	arduboy.display();
-	arduboy.drawBitmap(0, 8, TEAMarg, 128, 48, WHITE);
-	arduboy.display();
-	delay(3000);
+  arduboy.start();
+  arduboy.clearDisplay();
+  arduboy.display();
+  arduboy.drawBitmap(0, 8, TEAMarg, 128, 48, WHITE);
+  arduboy.display();
+  delay(3000);
 }
 
 void loop() {
-	arduboy.clearDisplay();
-	  	for(byte i = 1; i <= 12; i++){
-		arduboy.drawLine(63+(30*sin(d))*sin(-0+3.12+((6.28/12))*i),31+30*sin(0+4.72+((6.28/12))*i),63+(27*sin(d))*sin(-0+3.12+((6.28/12))*i),31+27*sin(0+4.72+((6.28/12))*i),1);
-	}
-	arduboy.drawLine(63,31,63+(20*sin(d))*sin(-a+3.1),31+20*sin(a+4.7),1);
-	arduboy.drawLine(63,31,63+(15*sin(d))*sin(-b+1.1),31+15*sin(b+2.6),1);
-	arduboy.drawLine(63,31,63+(10*sin(d))*sin(-c+5.3),31+10*sin(c+0.6),1);
+  arduboy.clearDisplay();
+  for (byte i = 1; i <= 12; i++) {
+    arduboy.drawLine(63 + (30 * sin(d))*sin(-0 + 3.12 + ((6.28 / 12))*i), 31 + 30 * sin(0 + 4.72 + ((6.28 / 12))*i), 63 + (27 * sin(d))*sin(-0 + 3.12 + ((6.28 / 12))*i), 31 + 27 * sin(0 + 4.72 + ((6.28 / 12))*i), 1);
+  }
+  arduboy.drawLine(63, 31, 63 + (20 * sin(d))*sin(-a + 3.1), 31 + 20 * sin(a + 4.7), 1);
+  arduboy.drawLine(63, 31, 63 + (15 * sin(d))*sin(-b + 1.1), 31 + 15 * sin(b + 2.6), 1);
+  arduboy.drawLine(63, 31, 63 + (10 * sin(d))*sin(-c + 5.3), 31 + 10 * sin(c + 0.6), 1);
 
-	if(millis() >= lastSecond){
-		a+=0.105;
-		lastSecond += secondInterval;
-	}
-	if(millis() >= lastMinute){
-		b+=0.105;
-		lastMinute += minuteInterval;
-		}
-	if(millis() >= lastHour){
-		c+=0.525;
-		lastHour += hourInterval;
-	}
+  if (millis() >= lastSecond) {
+    a += 0.105;
+    lastSecond += secondInterval;
+  }
+  if (millis() >= lastMinute) {
+    b += 0.105;
+    lastMinute += minuteInterval;
+  }
+  if (millis() >= lastHour) {
+    c += 0.525;
+    lastHour += hourInterval;
+  }
 
-	d+=0.01;
-	arduboy.display();
+  d += 0.01;
+  arduboy.display();
 }
